920780336c76fb3c177d2a6d52e1b264222cbcd0
Lan Luong <lan.luong@student.csulb.edu>
2019-03-05 12:56:39 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const outputDiv = document.getElementById('output')
+
+const ulBegin = '<ul>'
+const ulEnd = '</ul>'
+const liBegin = '<li>'
+const liEnd = '</li>'
+
+const org1_depts = [
+    {
+      name: 'accounting',
+      children: [
+        { name: 'accounting payable', children: [] },
+        { name: 'accounting receivable', children: [] },
+      ],
+    },
+    {
+      name: 'finance',
+      children: [],
+    },
+  ]  
+
+const org2_depts = [
+    {
+      name: 'accounting',
+      children: [
+        { name: 'accounting payable', children: [] },
+        {
+          name: 'accounting receivable',
+          children: [{ name: 'cash', children: [] }, { name: 'check', children: [] }],
+        },
+      ],
+    },
+    {
+      name: 'finance',
+      children: [{ name: 'investment', children: [] }],
+    },
+  ]
+    
+
